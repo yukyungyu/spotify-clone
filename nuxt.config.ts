76b97e4d5501fe7bd30030d412e85474c96e279f
ykkim <ykkim@dfy.co.kr>
2024-07-30 17:02:10 +0900

    :construction: 메타 태그 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,23 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Spotify - Web Player:Music for Listeners',
+      htmlAttrs: {
+        lang: 'ko',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Spotify clone study' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: 'https://open.spotifycdn.com/cdn/images/favicon16.1c487bff.png' }
+      ],
+    }
+  }
 })
 
