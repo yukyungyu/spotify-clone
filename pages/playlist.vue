9acc50569a309fe98df427e3266db051ae2e33d3
ykkim <ykkim@dfy.co.kr>
2024-08-02 12:27:43 +0900

    :construction: Search 페이지 작업중

---
+++ pages/playlist.vue
@@ -0,0 +1,10 @@
+<template>
+    <STitle>재생목록</STitle>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/search.vue
+++ pages/search.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>
-    search
-  </div>
+    <STitle>모두 둘러보기</STitle>
+
+      <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6 ml-4 mt-4">
+        <div v-for="item in categories.items">
+          <CategorySelect :category="item.name" :image="item.icons[0].url" :key="item.id"/>
+        </div>
+      </div>
 </template>
 
 <script setup>
+import { ref, toRefs } from 'vue'
+import { CommonStore } from '@/stores/pinia'
 
+const store = CommonStore(); 
+const { $axios } = useNuxtApp()
+
+const token = ref(null)
+token.value = store.accessToken
+
+const categories = reactive({
+  items: [],
+})
+
+// 📌 get all category
+const getAllCategory = async () => {
+    try {
+        const response = await $axios.get('https://api.spotify.com/v1/browse/categories?locale=ko_KR', {
+            headers: {
+                Authorization: `Bearer ${store.accessToken}`,
+            }
+        })
+        categories.items = response.data.categories.items
+        console.log(categories.items)
+    } catch (error) {
+        error.value = 'Failed to fetch category ' + error.message
+    }
+}
+
+onMounted(() => {
+    getAllCategory()
+})
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+body {
+  color: #fff;
+}
 </style>
