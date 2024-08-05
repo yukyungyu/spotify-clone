2902c92390e831510d262d1eea65ff1a5c56c8df
ykkim <ykkim@dfy.co.kr>
2024-08-05 16:37:49 +0900

    :construction: 로그인 기능 구현중

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
-<template>  
-  <div>  
-    ddd
-  </div>
+<template>
+  <Album />
+  <Artist />
 </template>
 
-<script setup> 
-definePageMeta({ middleware: ['auth']})
-// import { useRoute, useRouter } from 'vue-router' 
-// const route = useRoute()
-// const router= useRouter()
+<script setup>
+definePageMeta({ middleware: ['auth'] });
+// import { useRoute, useRouter } from 'vue-router';
+// const route = useRoute();
+// const router = useRouter();
 // const store = CommonStore();
 
-// onMounted(() => { 
-  // if(!store.isUser && route.path === "/"){
-    // router.push('/login')
-  // }
-// }) 
+// onMounted(() => {
+//   console.log('로그인?', store.isUser);
+//   if (!store.isUser && route.path === '/') {
+//     router.push('/login');
+//   }
+// });
 </script>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,71 +1,32 @@
-<template>   
-  <div class="flex bg-black  w-[100%] h-[100vh] items-center justify-center">
-    <button @click.prevent="Login" class="bg-white h-[40px] text-black rounded-full px-5 py-2.5 text-base hover:bg-[#1ed760]">로그인 하기</button> 
-  </div> 
+<template>
+  <div class="flex bg-black w-[100%] h-[100vh] items-center justify-center">
+    <button
+      @click.prevent="Login"
+      class="bg-white h-[40px] text-black rounded-full px-5 py-2.5 text-base hover:bg-[#1ed760]"
+    >
+      로그인 하기
+    </button>
+  </div>
 </template>
-<script setup> 
-definePageMeta({ 
-  layout: 'blank' 
-}) 
- 
-import { CommonStore } from '@/stores/pinia';
-import { useRouter } from 'vue-router'
+<script setup>
+definePageMeta({
+  layout: 'blank',
+});
 
-const loading = ref(true);
-const error = ref(null);  
+import { CommonStore } from '@/stores/pinia';
+
 const store = CommonStore();
-const { $axios } = useNuxtApp();
-const router = useRouter()
 
 const config = useRuntimeConfig();
 const clientId = config.public.spotifyClientID;
 const uri = config.public.spotifyURL;
 
-
 // 📌 로그인
 const Login = () => {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${uri}`;
-  console.log(authUrl)
-  
-  if(!store.isUser) {
+
+  if (!store.isUser) {
     window.location.href = authUrl;
   }
-};  
-
-// 📌 spotify 인증 코드로 인증 토큰 발급하기
-onMounted(() => { 
-  const params = new URLSearchParams(window.location.search);
-  const code = params.get('code');
-  console.log('code: ', code)
-
-  if (code) {
-    console.log('ssssss')
-     fetchAccessToken(code);
-  } else {
-    error.value = 'Authorization code not found';
-    loading.value = false;
-  }
-});
-
-
-// 📌 인증 토큰 가져오기
-const fetchAccessToken = async (code) => {
-  console.log('fetchAccessToken')
-  try {  
-    const result = await $axios.post('http://localhost:3001/api/login', {
-      code: code,
-    });
-    store.login({
-      accessToken: result.data.accessToken,
-      refreshToken: result.data.refreshToken,
-      isUser: true,
-    });  
-    console.log('33: ', store.isUser)
-    // router.push('/')
-  } catch (error) {
-    error.value = 'Authentication failed: ' + error.message;
-  }
 };
-
-
 </script>
