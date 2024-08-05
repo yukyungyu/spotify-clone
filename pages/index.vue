a20d94efd667da39768e1ef4ffd790c564dd350a
chomihye0924 <mihye024@dfy.co.kr>
2024-08-05 15:05:04 +0900

    :construction: router 삽질중

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>  
-  <div> 
-    <Album />  
-    <Artist /> 
+  <div>  
+    ddd
   </div>
 </template>
 
-<script setup>
+<script setup> 
+definePageMeta({ middleware: ['auth']})
+// import { useRoute, useRouter } from 'vue-router' 
+// const route = useRoute()
+// const router= useRouter()
+// const store = CommonStore();
 
+// onMounted(() => { 
+  // if(!store.isUser && route.path === "/"){
+    // router.push('/login')
+  // }
+// }) 
 </script>
 
 <style lang="css" scoped>
