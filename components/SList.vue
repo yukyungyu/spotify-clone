48ad8428af422bedbf629dece56f513ea89507fd
ykkim <ykkim@dfy.co.kr>
2024-08-06 19:20:17 +0900

    :construction: 동적 라우팅

--- components/SList.vue
+++ components/SList.vue
@@ -1,42 +1,53 @@
 <template>
   <section class="list_wrap">
     <ul ref="list" :class="`list_box ${type} ${borderRadius ? 'circle' : ''}`">
-      <li v-for="(item, index) in items" :key="index" class="hover:bg-zinc-900 px-2 py-2">
-        <a :href="item.href">
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        class="hover:bg-zinc-900 px-2 py-2"
+      >
+        <NuxtLink :to="{ path: `/${type}/${item.id}` }">
           <div :class="icon ? 'play_icon' : 'no_icon'">
             <img
-            class="rounded-sm"
-            :src="item.images[0]?.url"
-            :alt="item.name"
-            :width="
-              item.images[0]?.width !== null ? item.images[0]?.width : '100%'
-            "
-            :height="
-              item.images[0]?.height !== null ? item.images[0]?.height : '100%'
-            "
-          />
-          <button @click="play">재생</button>
+              class="rounded-sm"
+              :src="item.images[0]?.url"
+              :alt="item.name"
+              :width="
+                item.images[0]?.width !== null ? item.images[0]?.width : '100%'
+              "
+              :height="
+                item.images[0]?.height !== null
+                  ? item.images[0]?.height
+                  : '100%'
+              "
+            />
+            <button @click="play">재생</button>
           </div>
           <template v-if="type === 'album'">
-            <h3 class="title pt-5 pb-2">{{ item.name }}</h3>  
-            <p v-if="!date" class="add-info pb-5 text-gray-400 text-sm">{{ item.artists[0].name }}</p> 
+            <h3 class="title pt-5 pb-2">{{ item.name }}</h3>
+            <p v-if="!date" class="add-info pb-5 text-gray-400 text-sm">
+              {{ item.artists[0].name }}
+            </p>
             <p v-else class="add-info pb-5 text-gray-400 text-sm">
-              <span class="dot inline-block mr-5">{{ format(new Date(item.release_date), 'yyyy') }}</span>
+              <span class="dot inline-block mr-5">{{
+                format(new Date(item.release_date), 'yyyy')
+              }}</span>
               <span>{{ item.artists[0].name }}</span>
-            </p>  
+            </p>
           </template>
           <template v-if="type === 'artist'">
-            <h3 class="title pt-5 pb-2">{{ item.name }}</h3> 
-            <p class="add-info pb-5 text-gray-400 text-sm">{{ item.type === 'artist' ? '아티스트' : '' }}</p> 
-          </template> 
+            <h3 class="title pt-5 pb-2">{{ item.name }}</h3>
+            <p class="add-info pb-5 text-gray-400 text-sm">
+              {{ item.type === 'artist' ? '아티스트' : '' }}
+            </p>
+          </template>
           <template v-if="type === 'playlist'">
             <h3 class="title pt-5 pb-2">{{ item.name }}</h3>
             <p class="add-info pb-5 text-gray-400 text-sm">
               {{ ellipsis(item.description) }}
             </p>
           </template>
-
-        </a>
+        </NuxtLink>
       </li>
     </ul>
   </section>
@@ -64,15 +75,14 @@ const props = defineProps({
   },
   icon: {
     type: Boolean,
-    default: false, 
-  }
+    default: false,
+  },
 });
 const items = computed(() => props.data);
-const list = ref(null);    
-  
- 
+const list = ref(null);
+
 // 📌 resize, 마운트 설정
-onMounted(() => {  
+onMounted(() => {
   window.addEventListener('resize', adjustItems);
 });
 
@@ -81,38 +91,37 @@ onBeforeUnmount(() => {
   window.removeEventListener('resize', adjustItems);
 });
 
-
 // 📌 이미지 resize 처리
-const adjustItems = () => {  
-  if(!list.value) return false
- 
-  let totalWidth = 0; 
-  const containerWidth = list.value.clientWidth
+const adjustItems = () => {
+  if (!list.value) return false;
+
+  let totalWidth = 0;
+  const containerWidth = list.value.clientWidth;
   const childrenWidth = Array.from(list.value.children || []);
   const windowWidth = window.innerWidth;
 
   childrenWidth.forEach((item) => {
     item.classList.remove('hidden');
-  })
+  });
 
   childrenWidth.forEach((item) => {
-    totalWidth += item.clientWidth;    
+    totalWidth += item.clientWidth;
     if (totalWidth > containerWidth) {
       item.classList.add('hidden');
-    } 
+    }
     if (windowWidth <= 768) {
       if (childrenWidth.length > 0) {
         childrenWidth[0].classList.remove('hidden');
       }
     }
-  }); 
+  });
 };
 
 // 📌 데이터 내려받기
 watch(
   () => props.data,
-  async() => { 
-    await nextTick(); 
+  async () => {
+    await nextTick();
     adjustItems();
   },
   { immediate: true },
@@ -124,7 +133,6 @@ const ellipsis = (text, length = 42) => {
   }
   return text;
 };
-
 </script>
 
 <style lang="css" scoped>
