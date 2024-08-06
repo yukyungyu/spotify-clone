07b782b95176c85425ba225953324b76a052dea0
chomihye0924 <mihye024@dfy.co.kr>
2024-08-06 14:39:55 +0900

    :bug: [slist] 이미지 resize 처리 완료

--- components/SList.vue
+++ components/SList.vue
@@ -4,16 +4,15 @@
       <li v-for="(item, index) in items" :key="index" class="hover:bg-zinc-900">
         <a :href="item.href" class="">
           <img
-            class="px-5 py-5"
             :src="item.images[0]?.url"
             :alt="item.name"
             :width="item.images[0]?.width"
             :height="item.images[0]?.height"
           />
           <template v-if="type === 'album'">
-            <h3 class="px-5 pt-2">{{ item.name }}</h3>  
-            <p v-if="!date" class="px-5 p-2 text-gray-400 text-sm">{{ item.artists[0].name }}</p> 
-            <p v-else class="px-5 p-2 text-gray-400 text-sm">
+            <h3 class="px-5 pt-5">{{ item.name }}</h3>  
+            <p v-if="!date" class="px-5 pt-2 text-gray-400 text-sm">{{ item.artists[0].name }}</p> 
+            <p v-else class="px-5 pt-2 text-gray-400 text-sm">
               <span class="dot inline-block mr-5">{{ format(new Date(item.release_date), 'yyyy') }}</span>
               <span>{{ item.artists[0].name }}</span>
             </p>  
@@ -50,23 +49,11 @@ const props = defineProps({
   },
 });
 const items = computed(() => props.data);
-const list = ref(''); 
+const list = ref(null);    
+  
  
-
-// 📌 데이터 내려받기
-watch(
-  () => props.data,
-  async () => {
-    await nextTick();
-    adjustItems();
-  },
-  { immediate: true },
-);
-
 // 📌 resize, 마운트 설정
-onMounted(async () => {
-  await nextTick();
-  adjustItems();
+onMounted(() => {  
   window.addEventListener('resize', adjustItems);
 });
 
@@ -75,31 +62,43 @@ onBeforeUnmount(() => {
   window.removeEventListener('resize', adjustItems);
 });
 
-// items 이 resize 됐을 때 줄어들게 설정
-const adjustItems = () => {
-  if (!list.value) return;
 
-  let totalWidth = 0;
-  const containerWidth = list.value.offsetWidth;
-  const data = Array.from(list.value.children);
+// 📌 이미지 resize 처리
+const adjustItems = () => {  
+  if(!list.value) return false
+ 
+  let totalWidth = 0; 
+  const containerWidth = list.value.clientWidth
+  const childrenWidth = Array.from(list.value.children || []);
   const windowWidth = window.innerWidth;
 
-
-  data.forEach((item) => {
+  childrenWidth.forEach((item) => {
     item.classList.remove('hidden');
-  });
+  })
 
-  data.forEach((item) => {
-    totalWidth += item.offsetWidth; 
-    // console.log(totalWidth, containerWidth) 
-    if (totalWidth > containerWidth && totalWidth !== 0) {
+  childrenWidth.forEach((item) => {
+    totalWidth += item.clientWidth;   
+
+    if (totalWidth > containerWidth + 20) {
       item.classList.add('hidden');
+    } 
+    if (windowWidth <= 768) {
+      if (childrenWidth.length > 0) {
+        childrenWidth[0].classList.remove('hidden');
+      }
     }
-    if(windowWidth < 768) {
-      data[0].classList.remove('hidden');
-    }
-  });
+  }); 
 };
+
+// 📌 데이터 내려받기
+watch(
+  () => props.data,
+  async() => { 
+    await nextTick(); 
+    adjustItems();
+  },
+  { immediate: true },
+);
 </script>
 
 <style lang="css" scoped>
