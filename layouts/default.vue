4e0e803f897d2e21981e25d089473604db337b78
ykkim <ykkim@dfy.co.kr>
2024-08-06 10:36:54 +0900

    :art: 컨텐츠 영역 padding 조정

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <Header />
   <SideNav />
   <div
-    class="fixed right-0 top-0 w-[calc(100%-420px)] overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
+    class="content-area fixed right-0 top-0 w-[calc(100%-420px)] px-4 overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
   >
     <div class="mt-[70px]"></div>
     <slot />
