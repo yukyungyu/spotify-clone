230d306ab820a63599ed8e0bb4cda74314fe698a
ykkim <ykkim@dfy.co.kr>
2024-08-09 12:06:45 +0900

    :art: 컨텐츠 영역 -> main으로 변경, 스크롤바 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,14 @@
 <template>
   <Header />
   <SideNav />
-  <div
-    class="content-area fixed right-0 top-0 w-[calc(100%-360px)] px-4 pb-24 overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
+  <main
+    id="main"
+    class="fixed right-0 top-0 w-[calc(100%-360px)] px-4 pb-24 overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
   >
     <div class="mt-[70px]"></div>
     <slot />
     <div class="mb-100px"></div>
-  </div>
+  </main>
   <MusicPlayer />
 </template>
-<style>
-.content-area {
-  /* background: #484158; */
-  /* background: linear-gradient(90deg, #0e0425 0%, rgba(28, 28, 28, 1) 53%); */
-}
-</style>
+<style></style>
